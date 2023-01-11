a954768bf3f2855d1bcc843101bf91204ff178e6
Michael Chop <007micle007@gmail.com>
2023-01-11 13:44:34 +0300

    Added the ability to sort and filter pizzas

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,4 @@
-import {useState} from "react";
-
-function Categories() {
-    const [activeIndex, setActiveIndex] = useState(0)
+function Categories({activeIndex, setActiveIndex}) {
     const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
 
     return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,28 @@ const Home = () => {
     const [pizzas, setPizzas] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
+    const [categoryId, setCategoryId] = useState(0)
+    const [sortType, setSortType] = useState({
+        name: 'популярности', sortProperty: 'rating'
+    })
+    const [orderType, setOrderType] = useState(true);
+
     useEffect(() => {
-        fetch('https://63bbfb2fcf99234bfa6aa932.mockapi.io/items')
+        setIsLoading(true)
+        fetch(`https://63bbfb2fcf99234bfa6aa932.mockapi.io/items?${categoryId ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=${orderType ? 'desc' : 'asc'}`)
             .then(res => res.json())
             .then(json => {
                 setPizzas(json)
                 setIsLoading(false)
             })
         window.scrollTo(0, 0)
-    }, [])
+    }, [categoryId, sortType, orderType])
 
     return (
         <div className="container">
             <div className="content__top">
-                <Categories/>
-                <Sort/>
+                <Categories activeIndex={categoryId} setActiveIndex={setCategoryId}/>
+                <Sort selected={sortType} setSelected={setSortType} orderType={orderType} setOrderType={setOrderType}/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
